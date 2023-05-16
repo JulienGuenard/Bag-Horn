6525a1c403410d19bc0cfa85cee0bf1523cc7ce3
Archer01 <julienguenard01@gmail.com>
2023-05-16 12:08:11 +0200

    Bouton retour au menu
    - Bouton retour au menu pour redémarrer depuis l'écran de selection de niveau

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -10,14 +10,17 @@ public class GameManager : MonoBehaviour
     public string dialogScene_Good;
     public string dialogScene_Super1;
     public string dialogScene_Super2;
+    public string selectionLevelScene;
 
     public Button validateBtn;
     public Button resetBtn;
+    public Button returnToMenuBtn;
 
     public void Validate()
     {
         validateBtn.interactable = false;
         resetBtn.interactable = false;
+        returnToMenuBtn.interactable = false;
 
         if (SatisfactionManager.instance.character1_satisfaction >= SatisfactionManager.instance.character1.character.superEndingCount)
         {
@@ -45,5 +48,14 @@ public class GameManager : MonoBehaviour
     public void Reset()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+    }
+
+    public void ReturnToMenu()
+    {
+        if (validateBtn != null) validateBtn.interactable = false;
+        if (resetBtn != null) resetBtn.interactable = false;
+        returnToMenuBtn.interactable = false;
+
+        FadeToScene.instance.GoTo_SceneEnding(selectionLevelScene);
     }
 }
